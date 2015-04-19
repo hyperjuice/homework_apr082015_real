31f6054b549cd8da5d5507792cfeab74d87202fa
Darrell Cohn <darrellcohn@gmail.com>
2015-04-18 21:26:05 -0700

    Test

--- app.js
+++ app.js
@@ -13,10 +13,13 @@ app.get('/', function(req,res) {
 app.get('/search', function(req,res) {
   var queryParams = req.query.q;
   var url = "http://www.omdbapi.com?s="+queryParams;
+  // + means, the queryParams is added to the "xxxxxx"
   request(url, function(err,resp,body){
   	if (!err && resp.statusCode === 200) {
   		var jsonData = JSON.parse(body);
-  		res.render('search', { movies: jsonData.Search});
+  		res.render('search', { movies: jsonData.Search} // .Search is capitalized bc that's how it's done in the omdB response
+  		// 'movies' will be used on the search page, to display it	
+  		);
   	}
   });
 });
@@ -27,10 +30,14 @@ app.get('/movie', function(req,res) {
 	var url = "http://www.omdbapi.com?i="+imdbID;
 	request(url, function(err,resp,body) {
 		if (!err && resp.statusCode === 200) {
+// if u don't do the 'if', u could get an error if there is a prob w/omdB server, and then ull get an error returned/displayed. This statment prevents this possibility.
+
 			var movieData = JSON.parse(body);
 			res.render('movie', {movie: movieData});
+						// 1st, test the ejs with {movie: {Plot: "I'm plot", Title: "I'm a title"}}
+						// so Line 34 says, on the movie view, in the Plot and Title spots, put "I'm a plot" and "I'm a title". And the finished Line 34 says to fill it with actual results from the omdB request.
 		}
-	});
+	});					
 });
 
 app.listen(3000, function() {
